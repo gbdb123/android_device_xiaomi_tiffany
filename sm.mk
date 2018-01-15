670c090f3f5f2961bbdc82ed0052951f678f9b58
gbdb123 <747807389@qq.com>
2018-01-15 22:17:47 +0800

    sm-3.1

---
+++ sm.mk
@@ -0,0 +1,32 @@
+#
+# Copyright (C) 2017 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, device/xiaomi/tiffany/full_tiffany.mk)
+
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/sm/config/common_full_phone.mk)
+
+PRODUCT_NAME := sm_tiffany
+BOARD_VENDOR := Xiaomi
+
+
+PRODUCT_DEFAULT_LOCALE := zh_CN
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT="xiaomi/tiffany/tiffany:7.1.2/N2G47H/V9.0.3.0.NDBCNEI:user/release-keys" \
+    PRIVATE_BUILD_DESC="tiffany-user 7.1.2 N2G47H V9.0.3.0.NDBCNEI release-keys"
