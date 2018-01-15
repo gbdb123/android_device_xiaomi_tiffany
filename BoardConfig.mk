1e88bf622f917312af3f6d4f283bdd776efd8fc1
gbdb123 <747807389@qq.com>
2018-01-15 22:28:05 +0800

    modify use kernel source

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,9 +45,10 @@ BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
-#don't use kernel source
-#TARGET_KERNEL_CONFIG := tiffany_defconfig
-#TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+#don't use the bulit kernel
+TARGET_KERNEL_CONFIG := tiffany_defconfig
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
+# end
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel.gz
 
